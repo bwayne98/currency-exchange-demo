ef4dbefc7341e7bea606bb5cfb875449bfe8db52
wayne <bm.stamina@gmail.com>
2024-03-20 13:03:10 +0000

    新增 exchange 路由

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"net/http"
 
+	"github.com/bwayne98/currency-exchange-demo/service"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type ExchangeRequest struct {
@@ -24,6 +26,59 @@ func main() {
 
 	route.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World!")
+	})
+
+	service := service.CurrencyExchangeService{}
+	exchangeMap := map[string]map[string]float64{
+		"TWD": {
+			"TWD": 1,
+			"JPY": 3.669,
+			"USD": 0.03281,
+		},
+		"JPY": {
+			"TWD": 0.26956,
+			"JPY": 1,
+			"USD": 0.00885,
+		},
+		"USD": {
+			"TWD": 30.444,
+			"JPY": 111.801,
+			"USD": 1,
+		},
+	}
+
+	route.POST("exchange", func(c *gin.Context) {
+		var request ExchangeRequest
+		validate := validator.New()
+
+		if err := c.BindJSON(&request); err != nil {
+			c.JSON(http.StatusBadRequest, ExchangeResponse{
+				Msg: err.Error(),
+			})
+			return
+		}
+
+		if err := validate.Struct(&request); err != nil {
+			c.JSON(http.StatusBadRequest, ExchangeResponse{
+				Msg: err.Error(),
+			})
+			return
+		}
+
+		amount, err := service.Exchange(exchangeMap, request.Amount, request.Source, request.Target)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, ExchangeResponse{
+				Msg: err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, ExchangeResponse{
+			Msg:    "success",
+			Amount: amount,
+		})
+
 	})
 
 	route.Run(":80")
